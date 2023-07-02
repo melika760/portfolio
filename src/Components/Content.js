a31b711152f817457ccb1bbcab5d6454c6c7151d
melika760 <72795928+melika760@users.noreply.github.com>
2023-07-02 20:18:35 +0330

    save

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,15 +1,18 @@
-
+import logo from "../Icon.jpg"
 import classes from "./Content.module.css"
 export default function Content(){
     return(
 
-<di className={classes.content}>
-    <div>
-    <h3>Hello! I am Melika</h3>
+<div className={classes.content}>
+    <div className={classes.pr}>
+    <h1>Hello!I am Melika👋</h1>
     <p>Cutting-Edge Front End Wizard specializing in transforming ideas into visually stunning and interactive digital experiences. With a knack for blending artistry and functionality, I breathe life into designs through meticulous coding, leveraging the latest web technologies. A master of HTML, CSS, and JavaScript, I navigate the ever-evolving front end landscape with ease, while my passion for user-centric design ensures seamless usability across devices. Harnessing the power of responsive frameworks, I craft pixel-perfect websites that leave a lasting impression and elevate brands to new heights.</p>
     </div>
-    <div>image</div>
-</di>
+    <div>
+        <img src={logo} alt="girl"/>
+
+    </div>
+</div>
 
     )
 }
