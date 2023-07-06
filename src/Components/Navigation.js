0d6ce439896f7fb2025f6e9e35ee8ef28536308c
melika760 <72795928+melika760@users.noreply.github.com>
2023-07-06 17:54:40 +0330

    complete Nav

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,19 +1,18 @@
-import React,{useState} from "react"
+import React,{useState} from "react";
 import "./Navigation.css"
 export default function Navigation(){
     const[NavOpen,setNavOpen]=useState(false)
     function toggle(){
-        setNavOpen(true)
+        setNavOpen(!NavOpen)
     }
     return(
         <div className='nav'>
             <div className="nav-container">
                 <div className="navbar">
-                    <div className="logo">web Developer</div>
                     <div className='MenueToggle' onClick={toggle}>
                         <div className={NavOpen ? "Box BoxOpen" :"Box"}>
                             <span className={NavOpen ? "lineTop spin" : "lineTop"}></span>
-                            <span className="lineCenter"></span>
+                            <span className={NavOpen ? "lineCenter spin" : "lineCenter"}></span>
                             <span className={NavOpen ? "linebottom spin" : "linebottom"}></span>
                         </div>
                     </div>
@@ -23,7 +22,19 @@ export default function Navigation(){
                     {
                         top: NavOpen? "0" : "-100%",
                         transitionDelay:NavOpen? "0s":"0s"
-                    }}></div>
+                    }}>
+                      <div className="wrapper">
+                      <div >
+                            <a className="backLink" href="/">About</a>
+                        </div>
+                        <div >
+                            <a className="backLink" href="/">Projects</a>
+                        </div>
+                        <div>
+                            <a className="backLink" href="/">Contact</a>
+                        </div>
+                      </div>
+                    </div>
             </div>
         </div>
     )
