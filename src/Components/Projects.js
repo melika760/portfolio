97e3076b9d3b8ead3be269098c7d81ee88f96949
melika760 <72795928+melika760@users.noreply.github.com>
2023-07-11 15:54:07 +0330

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,36 +1,26 @@
-import React,{Fragment, useState} from "react";
-import weather from "./images/pngimg.com - iphone_PNG5735-fotor-20230706192623.jpg";
-import styles from "./Projects.module.css"
+import React,{Fragment} from "react";
+import styles from "./Projects.module.css";
+import Weather from "./images/pngimg.com - iphone_PNG5735-fotor-20230706192623.jpg"
 export default function Projects(){
-    const[hover,sethover]=useState(false)
-    const[text,setText]=useState()
-    function hoverhandler(){
-sethover(true)
-setText(<div>
-    <h3>WeatherApp</h3>
-    <h3>React</h3>
-    </div>)
-    }
-    function EndHover(){
-        sethover(false)
-    }
-
     return(
-<Fragment>
-    <h2>Projects</h2>
-    <div className={styles.col}>
-        <div className={styles.weather} onMouseOver={hoverhandler} onMouseOut={EndHover}>
-           {hover && text } <img src={weather} alt="WeatherApp"/>
-            </div>
-        <div className={styles.Dicationary}><img src={weather} alt="Dictionary"/></div>
-   
-    </div>
-    <div className={styles.col}>
-        <div className={styles.weather}><img src={weather} alt="ToDO"/></div>
-        <div className={styles.Dicationary}><img src={weather} alt="FoodApp"/></div>
-   
-    </div>
-   
-</Fragment>
+       <Fragment>
+            <div className={styles.col}>
+<div className={styles.weather}>
+    <img src={Weather} alt="Weather"/>
+</div>
+<div className={styles.weather}>
+    <img src={Weather} alt="Weather"/>
+</div>
+</div>
+<div className={styles.col}>
+<div className={styles.weather}>
+    <img src={Weather} alt="Weather"/>
+</div>
+<div className={styles.weather}>
+    <img src={Weather} alt="Weather"/>
+</div>
+</div>
+        </Fragment>
+        
     )
 }
