d8296907ede783ab64c0fb895a09e28185fd76d4
melika760 <72795928+melika760@users.noreply.github.com>
2023-07-11 16:38:53 +0330

    hover handler

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,14 @@
-import React,{Fragment} from "react";
+import React,{Fragment,useState} from "react";
 import styles from "./Projects.module.css";
-import Weather from "./images/pngimg.com - iphone_PNG5735-fotor-20230706192623.jpg"
+import Weather from "../weather-app night mood mobile.png"
 export default function Projects(){
+    const[hover,sethover]=useState(false)
+    function hovering(){
+        sethover(true)
+    }
+    function onHover(){
+        sethover(false)
+    }
     return(
        <Fragment>
         <h2>Projects</h2>
@@ -9,13 +16,13 @@ export default function Projects(){
 <div className={styles.weather}>
     <img src={Weather} alt="Weather"/>
 </div>
-<div className={styles.weather}>
+<div className={styles.weather} onMouseEnter={hovering} onMouseLeave={onHover}>
     <img src={Weather} alt="Weather"/>
-    <div className={styles.content}>
+   {hover && <div className={styles.content}>
         <h3>WeatherApp</h3>
         <p>Developing by React and Weather Api to help you track the weather and 10 days Forecast</p>
         <button>Check it!</button>
-    </div>
+    </div>}
 </div>
 </div>
 <div className={styles.col}>
