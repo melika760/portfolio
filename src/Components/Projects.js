8dfd39339b4ce54eb66c8b3268e956b2b914b951
melika760 <72795928+melika760@users.noreply.github.com>
2023-07-11 16:22:55 +0330

    edit css

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,12 +4,18 @@ import Weather from "./images/pngimg.com - iphone_PNG5735-fotor-20230706192623.j
 export default function Projects(){
     return(
        <Fragment>
+        <h2>Projects</h2>
             <div className={styles.col}>
 <div className={styles.weather}>
     <img src={Weather} alt="Weather"/>
 </div>
 <div className={styles.weather}>
     <img src={Weather} alt="Weather"/>
+    <div className={styles.content}>
+        <h3>WeatherApp</h3>
+        <p>Developing by React and Weather Api to help you track the weather and 10 days Forecast</p>
+        <button>Check it!</button>
+    </div>
 </div>
 </div>
 <div className={styles.col}>
