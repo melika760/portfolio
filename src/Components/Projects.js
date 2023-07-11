b67646deae94716072b4af8b03c9596cf74cba74
melika760 <72795928+melika760@users.noreply.github.com>
2023-07-11 19:32:17 +0330

    edit portfolio

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,12 +1,14 @@
 import React,{Fragment,useState} from "react";
 import styles from "./Projects.module.css";
 import Weather from "./images/pngimg.com - iphone_PNG5735-fotor-20230706192623.jpg";
-import ToDo from "./images/TodoList-20230711172625.jpg";
+import ToDo from "./images/Free-Laptop-Mockup-Psd-by-dailymockup-fotor-20230711181923.jpg";
+import Dictionary from "./images/iMac-on-Table-Free-PSD-Mockup-by-Helmii-Saputra-fotor-20230711184858.jpg"
 import Modal from "./Modal";
 import Button from "./UI/Button";
 export default function Projects(){
     const[Weatherhover,setweatherhover]=useState(false);
     const[Todohover,setTodohover]=useState(false);
+    const[dicthover,setdicthover]=useState(false)
 
     return(
        <Fragment>
@@ -31,8 +33,13 @@ export default function Projects(){
 </div>
 </div>
 <div className={styles.col}>
-<div className={styles.weather}>
-    <img src={Weather} alt="Weather"/>
+<div className={styles.Dicationary} onMouseEnter={()=>setdicthover(true)} onMouseLeave={()=>setdicthover(false)}>
+{dicthover &&  <Modal> <h3>Dictionary App</h3>
+    <p>You can find words definition</p>
+    <Button>CheckOut</Button>
+    <Button>GitHub</Button>
+    </Modal>}
+    <img src={Dictionary} alt="Dictionary"/>
 </div>
 <div className={styles.weather}>
     <img src={Weather} alt="Weather"/>
